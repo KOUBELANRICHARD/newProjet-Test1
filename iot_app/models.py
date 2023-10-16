c7dcd1970cf142d1ec58fd97e8120eb0838ba574
KOUBELANRICHARD <richard.akpagni@gmail.com>
2023-10-16 21:10:20 +0200

    Ajout du models.py avec les class Device et client

--- iot_app/models.py
+++ iot_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    full_name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
+
+class Device(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='devices')
+    name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200) # ex. Temperature, Humidity
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
